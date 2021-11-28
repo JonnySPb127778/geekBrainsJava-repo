efc95cc7781f2a9dea9b78d8e4832d36b0b58b4b
JonnySPb127778 <aei.spb@gmail.com>
2021-11-28 21:07:55 +0300

    Java Spring Homework 5

---
+++ JavaS_HW_5/java/com/geekbrains/spring/webjs/data/Product.java
@@ -0,0 +1,65 @@
+package com.geekbrains.spring.webjs.data;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "unit")
+    private String unit;
+
+    @Column(name = "cost")
+    private Float cost;
+
+
+    public Long getId() { return id; }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUnit() { return unit; }
+    public void setUnit(String unit) { this.unit = unit; }
+
+    public Float getCost() {
+        return cost;
+    }
+    public void setCost(Float cost) {
+        this.cost = cost;
+    }
+
+    public Product() {
+    }
+/*
+    public Product(Long id, String title, String unit, Float cost) {
+        this.id = id;
+        this.title = title;
+        this.unit = unit;
+        this.cost = cost;
+    }
+   */
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", unit='" + unit + '\'' +
+                ", cost=" + cost +
+                '}';
+    }
+
+}
---
+++ JavaS_HW_5/java/com/geekbrains/spring/webjs/repositories/SessionFactoryUtils.java
@@ -0,0 +1,18 @@
+package com.geekbrains.spring.webjs.repositories;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SessionFactoryUtils {
+    private SessionFactory factory = new Configuration()
+            .configure("hibernate.cfg.xml")
+            .buildSessionFactory();
+
+    public Session getSession() {
+        return factory.getCurrentSession();
+    }
+
+}
